e3bb992bd49235cb3a8bd16e3ccfd9abba0e1bea
nickfriend13 <nickfriend13@icloud.com>
2025-11-24 15:32:31 +0000

    repaired moviebuilder.cs

--- MovieBuilder.cs
+++ MovieBuilder.cs
@@ -1,32 +1,32 @@
-namespaceq  Neo4JDotNetAttempt1.Models
+namespace  Neo4JDotNetAttempt1.Models
 {
-public class MovieBuilder
-{
-    private string _title = "Untitled";
-    private long _released = 0;
-    private string _tagline = "";
-
-    public MovieBuilder setTitle(string title)
+    public class MovieBuilder
     {
-        _title = title;
-        return this;
-    }
+        private string _title = "Untitled";
+        private long _released = 0;
+        private string _tagline = "";
 
-    public MovieBuilder setReleased(long released)
-    {
-        _released = released;
-        return this;
-    }
+        public MovieBuilder setTitle(string title)
+        {
+            _title = title;
+            return this;
+        }
 
-    public MovieBuilder setTagline(string tagline)
-    {
-        _tagline = tagline;
-        return this;
-    }
+        public MovieBuilder setReleased(long released)
+        {
+            _released = released;
+            return this;
+        }
 
-    public Movie Build()
-    {
-        return new Movie(_title, _released, _tagline);
+        public MovieBuilder setTagline(string tagline)
+        {
+            _tagline = tagline;
+            return this;
+        }
+
+        public Movie Build()
+        {
+            return new Movie(_title, _released, _tagline);
+        }
     }
-}
 }
